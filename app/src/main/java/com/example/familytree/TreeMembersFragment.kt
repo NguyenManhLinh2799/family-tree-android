48b3765f5d7198ac6d47bb2e9881619c1f123fc0
LINH NGUYỄN MẠNH <nmlinh2799@gmail.com>
2021-04-15 20:37:19 +0700

    Custom item view

--- app/src/main/java/com/example/familytree/TreeMembersFragment.kt
+++ app/src/main/java/com/example/familytree/TreeMembersFragment.kt
@@ -1,11 +1,11 @@
 package com.example.familytree
 
-import android.location.Location
-import android.opengl.Visibility
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -26,22 +26,39 @@ class TreeMembersFragment: Fragment() {
 
         val treeView = view.findViewById<MindMappingView>(R.id.treeView)
 
-        val father = Item(this.context, "Bố", "", true)
-        val mother = Item(this.context, "Mẹ", "", true)
-        val me = Item(this.context, "Tôi", "", true)
-        val brother = Item(this.context, "Em trai", "", true)
+        val family = Item(context, ".", "", false)
+
+
+        val father = Item(context, "Bố", "", false)
+        val mother = Item(context, "Mẹ", "", false)
+        val me = Item(context, "Tôi", "", false)
+        val brother = Item(context, "Em trai", "", false)
+        val sister = Item(context, "Chị gái", "", false)
 
         allMembers = listOf(
                 father,
                 mother,
                 me,
-                brother
+                brother,
+                sister
         )
 
-        treeView?.addCentralItem(father, false)
-        treeView.addItem(mother, father, 200, 200, ItemLocation.RIGHT, false, null)
-        treeView.addItem(me, father, 200, 200, ItemLocation.BOTTOM, false, null)
-        treeView.addItem(brother, father, 200, 200, ItemLocation.BOTTOM, false, null)
+        val memberIsMale = listOf<Boolean>(
+                true,
+                false,
+                true,
+                true,
+                false
+        )
+
+        treeView?.addCentralItem(family, false)
+        setFamilyStyle(family)
+
+        treeView.addItem(father, family, 300, 0, ItemLocation.LEFT, false, null)
+        treeView.addItem(mother, family, 300, 0, ItemLocation.RIGHT, false, null)
+        treeView.addItem(me, family, 300, 150, ItemLocation.BOTTOM, false, null)
+        treeView.addItem(brother, family, 300, 150, ItemLocation.BOTTOM, false, null)
+        treeView.addItem(sister, family, 300, 150, ItemLocation.BOTTOM, false, null)
 
         val memberInfoBtn = view.findViewById<LinearLayout>(R.id.memberInfoBtn)
         memberInfoBtn.setOnClickListener {
@@ -50,7 +67,10 @@ class TreeMembersFragment: Fragment() {
 
         val memberMenuBar = view.findViewById<LinearLayout>(R.id.memberMenuBar)
         memberMenuBar.visibility = View.INVISIBLE
-        allMembers.forEach { member ->
+
+        allMembers.forEachIndexed { index, member ->
+            setStyle(member, memberIsMale[index])
+
             member.setOnClickListener {
                 memberMenuBar?.visibility = when (memberMenuBar.visibility) {
                     View.INVISIBLE -> View.VISIBLE
@@ -59,5 +79,32 @@ class TreeMembersFragment: Fragment() {
                 }
             }
         }
+    }
+
+    private fun setStyle(item: Item, isMale: Boolean) {
+        item.setBackgroundResource(when (isMale) {
+            true -> R.drawable.bg_male
+            else -> R.drawable.bg_female
+        })
+
+        val image = ImageView(context)
+        image.setImageResource(when (isMale) {
+            true -> R.drawable.ic_male
+            else -> R.drawable.ic_female
+        })
+        image.setPadding(0, 0, 0, 20)
+        item.addView(image, 0)
+
+        item.gravity = Gravity.CENTER
+        item.title.gravity = Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM
+        item.removeView(item.content)
+
+        item.setPadding(5, 5, 5, 5)
+    }
+
+    private fun setFamilyStyle(family: Item) {
+        val params = family.layoutParams
+        params.height = 200
+        family.layoutParams = params
     }
 }
